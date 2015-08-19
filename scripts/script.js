71f4e7c2145868e61ae5fe359cbcfcc493d740c5
Esteban Gerpe Viéitez <stbn694@gmail.com>
2015-08-19 18:10:51 +0200

    Revert "Second page load test"
    This reverts commit 81c3338c83bbb1781e50e1ca06baf0b43a0d8c31.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,8 @@
 window.onload = function() {
-	$('*').show();
 	$('#loading').hide();
 }
 
 $(document).ready(function () {
-
-	$('*').hide();
-	$('#loading').show();
 
 	/* Navigation bar smooth scroll */
 
